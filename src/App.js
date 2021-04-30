f61d3ade3ae4508ce484569c7d8c8f7a9ca0e960
ArthurNNN <nikitinarthur@gmail.com>
2021-04-30 18:57:12 +0200

    Lesson 1.3 finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React from "react";
 import Heading from "./Heading";
+import TouristInfoCards from "./TouristInfoCards";
 import Bookings from "./Bookings";
 import "./App.css";
 
 const App = () => {
+  var cities = [
+    {
+      name: "Glasgow",
+      imgUrl: "https://media.timeout.com/images/105396642/image.jpg",
+      text:
+        "Explore the city's must-see sights, cultural attractions, amazing shopping, vibrant nightlife and more - make your next stop Glasgow!",
+      citySiteUrl: "https://peoplemakeglasgow.com"
+    },
+    {
+      name: "London",
+      imgUrl:
+        "https://cdn.londonandpartners.com/visit/london-organisations/houses-of-parliament/63950-640x360-london-icons2-640.jpg",
+      text:
+        "Find things to do in London, from iconic sightseeing spots and fun-filled days out to top restaurants, theatre and unmissable London events. If you’re not able to visit just yet, plan ahead to make the most of your next visit.",
+      citySiteUrl: "https://visitlondon.com"
+    },
+    {
+      name: "Manchester",
+      imgUrl:
+        "https://absolutely.london/wp-content/uploads/2017/05/MANCHESTER-HERO-1068x712.jpg",
+      text:
+        "Youthful, diverse, energetic and bursting with character; Manchester is one of the most exciting places to visit in the UK right now where everybody and anybody is very warmly welcomed.",
+      citySiteUrl: "https://visitmanchester.com/"
+    }
+  ];
   return (
     <div className="App">
       <Heading />
+      <TouristInfoCards citiesArr={cities} />
       <Bookings />
     </div>
   );
