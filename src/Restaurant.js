bf2c46975debe20a0e7bfc78af4d77e3ffbb0962
ArthurNNN <nikitinarthur@gmail.com>
2021-05-07 18:26:06 +0200

    Lesson 2 ex. 12 finished

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,21 +1,12 @@
-import React, { useState } from "react";
-import RestaurantButton from "./RestaurantButton";
+import React from "react";
+import Order from "./Order";
 
 const Restaurant = () => {
-  var [orders, setOrders] = useState(0);
-
-  const orderOne = () => {
-    setOrders(orders + 1);
-    console.log(orders);
-  };
-
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
-        <li>
-          Pizzas: {orders} <RestaurantButton handleClick={orderOne} />
-        </li>
+        <Order />
       </ul>
     </div>
   );
