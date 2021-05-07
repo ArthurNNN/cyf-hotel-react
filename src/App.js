8237135fb46957df801e9e8f2f63090c382a2937
ArthurNNN <nikitinarthur@gmail.com>
2021-05-07 17:45:56 +0200

    Lesson 2 ex.8 finished

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Heading from "./Heading";
 import TouristInfoCards from "./TouristInfoCards";
 import Bookings from "./Bookings";
+import Restaurant from "./Restaurant";
 import Footer from "./Footer";
 import "./App.css";
 
@@ -41,6 +42,7 @@ const App = () => {
       <Heading />
       <TouristInfoCards citiesArr={cities} />
       <Bookings />
+      <Restaurant />
       <Footer footerArr={footerArr} />
     </div>
   );
